c395638821938974c88c653843a1f1c52f80da3d
Ghanashyam <ghanashyamvtatti@gmail.com>
2020-05-09 19:27:15 -0400

    Enable CORS in API

--- backend/api.py
+++ backend/api.py
@@ -3,8 +3,11 @@ from flask import Flask
 from flask import request
 from utils.kafkahelper import KafkaConnection
 from threading import Thread
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 STATUS = 'READY'
 
