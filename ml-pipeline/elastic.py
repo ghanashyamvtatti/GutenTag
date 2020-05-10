cb3cf888bb38831c5dccb3faab856de8db266058
nikki2606 <nikitamj2606@gmail.com>
2020-05-10 15:54:14 -0400

    ML pipeline commit

---
+++ ml-pipeline/elastic.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import mlpipeline
+from elasticsearch import Elasticsearch
+from utils.kafkahelper import KafkaConnection
+
+def main():
+    conn = KafkaConnection()
+    #download es and keep it running on the local sys while executing this
+    es = Elasticsearch([{'host':'localhost', 'port':9200}])
+    #data = "717_20170904123036/717_webhose-2017-01_20170904123520/blogs_0000001.json"
+    id = 0
+    #set up of index for es
+    index = 'articles'
+    doc_type = 'article'
+
+    for data in conn.get_data():
+        detail = mlpipeline.write_to_json(data)
+        #insert json into elasticsearch
+        id += 1
+        store = es.index(index=index, doc_type=doc_type, id=id, body=detail)
+        print(store)
+        #just to retrieve data from es
+        retrieve = es.get(index=index, doc_type=doc_type, id=id)
+        print(retrieve['_source'])
+        #deleting the document(this statement can be deleted later)
+        erase = es.delete(index=index, doc_type=doc_type, id=id)
+        print(erase['result'])
+main()
---
+++ ml-pipeline/mlpipeline.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from pyspark.sql import SparkSession
+from pyspark.context import SparkContext
+from pyspark.conf import SparkConf
+from pyspark.sql.functions import * 
+from pyspark.sql.types import *
+from pyspark.ml.feature import StopWordsRemover, CountVectorizer, IDF, Tokenizer
+from pyspark.ml.clustering import LDA
+from pyspark.ml import Pipeline
+import json
+import nltk
+nltk.download('vader_lexicon')
+import nltk.data
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#sentiment analyzer function
+def sentiment_analysis(data):
+    analyzer = SentimentIntensityAnalyzer()
+    score = analyzer.polarity_scores(data)
+    if score['compound'] >= 0.05:
+        return 'positive'
+    elif score['compound'] <= -0.05:
+        return 'negative'
+    else:
+        return 'neutral'
+
+#pyspark creating spark session function
+def create_spark_session():
+    cf = SparkConf()
+    spark = SparkSession.builder.config(conf = cf).getOrCreate()
+    return spark
+
+#ml pipeline
+def ml_pipeline(jsondata):
+    spark = create_spark_session()
+    df = spark.read.json(jsondata)
+    data = df.select('url', 'text').where(df['text'] != '')
+
+    #sentiment analysis
+    text_sentiment = data.select('text').head()['text']
+    sentiment_fact = sentiment_analysis(text_sentiment)
+
+    #hyper-parameters
+    num_topics = 10
+    max_iterations = 100
+
+    #pipeline
+    tokenizer = Tokenizer(inputCol="text", outputCol="tokenized")
+    cleaned = StopWordsRemover(inputCol="tokenized", outputCol="cleaned")
+    tf = CountVectorizer(inputCol="cleaned", outputCol="raw_features", vocabSize=10, minDF=1.0)
+    idf = IDF(inputCol="raw_features", outputCol="features")
+    lda_model = LDA(featuresCol="features", k=num_topics, maxIter=max_iterations)
+    pipeline = Pipeline(stages=[tokenizer, cleaned, tf, idf, lda_model])
+
+    #clustering
+    model = pipeline.fit(data)
+    topics = model.stages[-1].describeTopics(1)
+    vocab = model.stages[2].vocabulary
+    url = data.select('url').head()['url']
+    return topics, vocab, url, sentiment_fact
+
+#write into json
+def write_to_json(jsondata):
+    topics, vocab, url, sentiment_fact = ml_pipeline(jsondata)
+    detail = {'url': url, 'vocab': vocab, 'sentiment': sentiment_fact}
+    ##ignore the 2 comments below since that was just test to dump json code into json file(only reqd if gotta create a json file)
+    #with open('file1.json', 'w') as filejson:
+    #    json.dump(detail, filejson)
+    return detail
